cf680a0c266300c22eaa88da7bfd828d51c250b8
Behdad <sadeghianbehdad@gmail.com>
2024-02-21 13:23:26 +0330

    Update train.py

--- train.py
+++ train.py
@@ -25,10 +25,10 @@ if __name__ == "__main__":
 
     Init_Epoch          = 0
     Freeze_Epoch        = 0
-    Freeze_batch_size   = 16
+    Freeze_batch_size   = 8
 
     UnFreeze_Epoch      = 100
-    Unfreeze_batch_size = 16
+    Unfreeze_batch_size = 8
 
     Freeze_Train        = False
     
